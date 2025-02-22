dfd6bdebfd74a06d3501b452c125a279a192e120
Sumrian Shi <3092787775@qq.com>
2025-02-22 15:10:42 +0800

    feat: 增加实用工具ai对话与在线编程页面

---
+++ src/views/uTool/OnlineProgramming/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <IFrame :frameSrc="url" />
+</template>
+
+<script setup lang="ts">
+  import IFrame from '@/layouts/components/iframe/index.vue';
+  import { ref } from 'vue';
+
+  defineOptions({ name: 'Admin' });
+
+  const url = ref<string>('https://jsfiddle.net/');
+</script>
+
+<style scoped></style>
